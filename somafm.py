9ca105d18e1ac718293e3b0af7369acc96231d00
tranquillo <rob.tranquillo@gmx.de>
2022-08-11 11:55:05 +0200

    update scraper

--- somafm.py
+++ somafm.py
@@ -1,23 +1,17 @@
 import requests
 import os
+from bs4 import BeautifulSoup
 
 #configuration
 outputDir = "SomaFM"
-source = "http://somafm.com/listen/index.html"
-source = "http://somafm.com/listen/genre.html"
-generateSonataConfig = False #to add in Sonata (~/.config/sonatarc)
-sonataConfigIterator = 0   #number of the next station in config file (starts with zero)
+source = "https://somafm.com/listen/"
 
 #todo: die Config parameter in der Shell übergeben
 
 #### Main
 def main():
   stations = getStationsByFormat(source)
-  print(str(len(stations['aac'])) + " AAC Listen eingelesen")
-  print(str(len(stations['mp3'])) + " MP3 Listen eingelesen")
-
-  toMultiFile(stations['mp3'], 'somafm-mp3.m3u')
-  toMultiFile(stations['aac'], 'somafm-aac.m3u')
+  print(str(len(stations['pls'])) + " *.pls Listen eingelesen")
 
   try:
     os.mkdir(outputDir)
@@ -25,29 +19,9 @@ def main():
     print ("Creation of the directory %s failed" % outputDir)
   else:
     print ("Successfully created the directory %s " % outputDir)
-        
 
-
-  for station in stations['mp3']:
+  for station in stations['pls']:
     toFile(station, outputDir+'/somafm - '+station['name']+'.m3u')
-
-
-  if generateSonataConfig:
-    extendSonata(stations['mp3'], sonataConfigIterator)
-
-
-
-'''
-  Support for the good old Sonata player
-
-  Insert the output to your sonata config to get the playlists to your Sonata
-'''
-def extendSonata(stations, sonataConfigIterator):
-  configString = ''
-  for station in stations:
-    configString = configString + "names["+str(sonataConfigIterator)+"] = soma fm "+station['name']+"\nuris["+str(sonataConfigIterator)+"] = "+station['url']+"\n"
-    sonataConfigIterator += 1
-  print('\n\n' + 'Put the following lines to ~/.config/sonatarc for Sonata Playlist Support. (To append Sonata List edit the interator in somafm.py):\n'+configString);
 
 
 '''
@@ -57,18 +31,19 @@ def extendSonata(stations, sonataConfigIterator):
   @return: a dict with name and url of a station
 '''
 def getStationsByFormat(source):
+  PLS = []
   r = requests.get(source)
-  AACLinks = []
-  MP3Links = []
-  for line in r.text.split('\n'):
-    if '<h3>' in line:
-      headline = line[4:line.find('</h3>')]
-    if '<a href' in line and '.pls' in line:
-      if 'AAC:' in line:
-        AACLinks.append({'name':headline, 'url':scrapeUrl(line)})
-      if 'MP3:' in line:
-        MP3Links.append({'name':headline, 'url':scrapeUrl(line)})
-  return { 'aac' : AACLinks, 'mp3' : MP3Links }
+  soup = BeautifulSoup(r.text, 'html.parser')
+  allLinks = soup.find_all('a')
+  for link in allLinks:
+    url = link.get('href')
+    if ("130.pls" not in url):
+      continue
+    if ("nossl/" in url):
+      continue
+    name = link.parent.parent.parent.previous_sibling.previous_sibling.previous_sibling.previous_sibling.text
+    PLS.append({"name": name, "url": "https://somafm.com" + url})
+  return { 'pls' : PLS }
 
 def scrapeUrl(line):
   start = line.find('<a href') +9
@@ -82,17 +57,6 @@ def toFile(station, filename):
   all.write(station['url']+ "\n\n")
   all.close()
   print('write '+filename)
-
-def toMultiFile(stations, filename):
-  success = 0
-  all = open(filename, 'w')
-  all.write("#EXTM3U\n\n")
-  for station in stations:
-    all.write("#EXTINF:"+str(success)+","+station['name']+"\n")
-    all.write(station['url']+ "\n\n")
-    success += 1
-  all.close()
-  print('write '+str(success)+' stations to '+filename)
 
 #start main function
 main()
